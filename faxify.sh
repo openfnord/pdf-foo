46e6071c4a96305928bbc3edbd76e4101117933d
openfnord <83826561+openfnord@users.noreply.github.com>
2025-01-26 18:07:52 +0100

    Update faxify.sh
    just added explanation why one needs faxify

--- faxify.sh
+++ faxify.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Script to convert a PDF to a fax-style graphic-only version without text.
+# Script to convert a PDF to a fax-style graphic-only version without text
+#
+# Why: faxify fakes pdf to be faxes faxify is needed if you want to send a "copy" of your fax via email, 
+# and you want to omit the discussion if a computer fax is a "real" fax, because it has pdf letters and only a signature picture
+# faxify "scans" your fax to g4 fax tiff and converts it back to pdf. So the document was a fax for legal purposes and looks alike:-)
+#
 
 # Check if the input file is provided
 if [ -z "$1" ]; then
